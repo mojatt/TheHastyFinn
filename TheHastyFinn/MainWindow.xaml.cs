ec4fe096ef448f48e304041430d559b2cc0f6592
Joe Hasty <hastyj@esi.com>
2016-08-03 11:26:48 -0700

    Initial commit of XFactor. Successful download of FScore stocks. Next is to parse the CSV. Added NuGet library to help.

--- TheHastyFinn/MainWindow.xaml.cs
+++ TheHastyFinn/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TheHastyFinn
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            XFactor xfactor = new XFactor();
+        }
     }
 }
---
+++ TheHastyFinn/XFactor/FScore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHastyFinn
+{
+    class FScore
+    {
+        private string _url = @"https://docs.google.com/spreadsheets/d/1GMU1-wmxpiruXEZd8gqYh8E0RZxw6pXw6Z5Pl7svA_4/pub?single=true&gid=15&output=csv";
+
+        public FScore()
+        {
+            GetCSV();
+        }
+
+        private void GetCSV()
+        {
+            WebCSVHandler wc = new WebCSVHandler(new System.Net.CookieContainer());
+            wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0");
+            wc.Headers.Add("DNT", "1");
+            wc.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            wc.Headers.Add("Accept-Encoding", "deflate");
+            wc.Headers.Add("Accept-Language", "en-US,en;q=0.5");
+
+            byte[] dt = wc.DownloadData(_url);
+            var outputCSVdata = System.Text.Encoding.UTF8.GetString(dt ?? new byte[] { });
+
+        }
+    }
+}
